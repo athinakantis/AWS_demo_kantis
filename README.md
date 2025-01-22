9fc91e2abc8b24a6fab93a3c11307ea622f726d6
Athina <athina.kantis@gmail.com>
2025-01-22 14:30:42 +0200

    update readme, app

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Steps
 
 1. Create project
+2. Upload the project to a github repo
+3. Connect github repo with AWS Amplify
--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <h1>AWS Demo</h1>
         <p>This is an AWS demo by Athina</p>
         <img src="/meow.jpg" alt="Cat by computer" />
+        <p>This line was added after the initial deploy to AWS</p>
       </div>
     </>
   )
