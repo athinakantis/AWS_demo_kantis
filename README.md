114f60d12212f6f5615ea17691bbee4b65aaa000
Athina <athina.kantis@gmail.com>
2025-01-22 14:00:08 +0200

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# AWS Demo
+
+## Steps
+
+1. Create project
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>AWS Demo</h1>
+        <p>This is an AWS demo by Athina</p>
+        <img src="/meow.jpg" alt="Cat by computer" />
+      </div>
+    </>
+  )
+}
+
+export default App
